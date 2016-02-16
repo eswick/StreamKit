4b1aba97e3341502d7fd17d686262cadc05c9043
Evan Swick <eswick@eswick.com>
2016-02-16 08:29:32 -0500

    Add 8-bit int read/write functions

--- Sources/Stream.swift
+++ Sources/Stream.swift
@@ -4,3 +4,21 @@ public protocol Stream {
     func write(bytes: [UInt8]) throws -> Int
     func close() throws
 }
+
+extension Stream {
+    func readUInt8() throws -> UInt8 {
+        return try read(1)[0]
+    }
+    
+    func readInt8() throws -> Int8 {
+        return try read(1)[0]
+    }
+    
+    func writeInt8(int8: Int8) throws {
+        try write([int8])
+    }
+    
+    func write(uint8: UInt8) throws {
+        try write([uint8])
+    }
+}
